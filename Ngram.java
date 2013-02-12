f66cc3ada2a1682b69c0491498982cbca0a88a2c
Botao Hu <me@botao.hu>
2013-02-11 23:14:54 -0800

    adad

--- Ngram.java
+++ Ngram.java
@@ -27,22 +27,22 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 public class Ngram {
 
 	static String stringJoin(Collection<?> s, String delimiter) {
-	     StringBuilder builder = new StringBuilder();
-	     Iterator<?> iter = s.iterator();
-	     while (iter.hasNext()) {
-	         builder.append(iter.next());
-	         if (!iter.hasNext()) {
-	           break;                  
-	         }
-	         builder.append(delimiter);
-	     }
-	     return builder.toString();
-	 }
-	
+		StringBuilder builder = new StringBuilder();
+		Iterator<?> iter = s.iterator();
+		while (iter.hasNext()) {
+			builder.append(iter.next());
+			if (!iter.hasNext()) {
+				break;
+			}
+			builder.append(delimiter);
+		}
+		return builder.toString();
+	}
+
 	public static class NgramMapper extends
 			Mapper<LongWritable, Text, NullWritable, TextIntPair> {
-		
-		private HashSet<String> query; 
+
+		private HashSet<String> query;
 		private TreeSet<TextIntPair> treeSet;
 		private int n;
 		private int k;
@@ -50,12 +50,13 @@ public class Ngram {
 		@Override
 		protected void setup(Context context) {
 			Configuration conf = context.getConfiguration();
-			query = new HashSet<String>(Arrays.asList(conf.get("query").split(",")));
+			query = new HashSet<String>(Arrays.asList(conf.get("query").split(
+					",")));
 			n = conf.getInt("n", 0);
 			k = conf.getInt("k", 0);
 			treeSet = new TreeSet<TextIntPair>();
 		}
-	
+
 		@Override
 		protected void map(LongWritable key, Text value, Context context)
 				throws IOException, InterruptedException {
@@ -64,12 +65,12 @@ public class Ngram {
 			Pattern titlePattern = Pattern.compile("<title>(.*?)</title>");
 			Matcher titleMatcher = titlePattern.matcher(page);
 			if (titleMatcher.find() && (titleMatcher.groupCount() > 0)) {
-				 title = titleMatcher.group(1);
-				 page = page.substring(titleMatcher.end());
+				title = titleMatcher.group(1);
+				page = page.substring(titleMatcher.end());
 			}
 			if (title == null)
 				return;
-			
+
 			Tokenizer tokenizer = new Tokenizer(page);
 			LinkedList<String> ngram = new LinkedList<String>();
 			int cnt = 0;
@@ -77,7 +78,7 @@ public class Ngram {
 				String token = tokenizer.next();
 				ngram.addLast(token);
 				while (ngram.size() > n) {
-					ngram.removeFirst(); 
+					ngram.removeFirst();
 				}
 				if (ngram.size() == n) {
 					if (query.contains(stringJoin(ngram, " "))) {
@@ -85,20 +86,20 @@ public class Ngram {
 					}
 				}
 			}
-			if (cnt > 0) {
-				treeSet.add(new TextIntPair(new Text(title), new IntWritable(cnt)));
-				while (treeSet.size() > k) {
-					treeSet.remove(treeSet.last());
-				}
+			treeSet.add(new TextIntPair(new Text(title), new IntWritable(cnt)));
+			while (treeSet.size() > k) {
+				treeSet.remove(treeSet.last());
 			}
 		}
-		protected void cleanup(Context context) {
+
+		protected void cleanup(Context context) throws IOException,
+				InterruptedException {
 			for (TextIntPair topPage : treeSet) {
 				context.write(NullWritable.get(), topPage);
 			}
 		}
 	}
-	
+
 	public static class NgramReducer extends
 			Reducer<NullWritable, TextIntPair, Text, IntWritable> {
 		private TreeSet<TextIntPair> treeSet;
@@ -109,6 +110,7 @@ public class Ngram {
 			k = context.getConfiguration().getInt("k", 0);
 			treeSet = new TreeSet<TextIntPair>();
 		}
+
 		@Override
 		public void reduce(NullWritable key, Iterable<TextIntPair> values,
 				Context context) throws IOException, InterruptedException {
@@ -119,17 +121,21 @@ public class Ngram {
 				}
 			}
 		}
+
 		@Override
-		protected void cleanup(Context context) throws IOException, InterruptedException {
+		protected void cleanup(Context context) throws IOException,
+				InterruptedException {
 			for (TextIntPair topPage : treeSet) {
 				context.write(topPage.getFirst(), topPage.getSecond());
 			}
 		}
 	}
-	
-	public static HashSet<String> readQuery(int n, Path file, Configuration conf) throws IOException {
+
+	public static HashSet<String> readQuery(int n, Path file, Configuration conf)
+			throws IOException {
 		FileSystem fs = file.getFileSystem(conf);
-		BufferedReader reader = new BufferedReader(new InputStreamReader(fs.open(file), "UTF-8"));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				fs.open(file), "UTF-8"));
 		StringBuilder sb = new StringBuilder();
 		String line;
 		while ((line = reader.readLine()) != null) {
@@ -144,7 +150,7 @@ public class Ngram {
 			String token = tokenizer.next();
 			ngram.addLast(token);
 			while (ngram.size() > n) {
-				ngram.removeFirst(); 
+				ngram.removeFirst();
 			}
 			if (ngram.size() == n) {
 				query.add(stringJoin(ngram, " "));
@@ -152,24 +158,24 @@ public class Ngram {
 		}
 		return query;
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
-		
+
 		FileSystem fs = new Path(args[3]).getFileSystem(conf);
 		fs.delete(new Path(args[3]), true);
-		
-		
+
 		int n = Integer.parseInt(args[0]);
 		conf.setInt("n", n);
 		conf.setInt("k", 20);
-		conf.set("query", stringJoin(readQuery(n, new Path(args[1]), conf), ","));
+		conf.set("query",
+				stringJoin(readQuery(n, new Path(args[1]), conf), ","));
 		conf.set("xmlStart", "<page>");
 		conf.set("xmlEnd", "</page>");
-		
+
 		Job job = new Job(conf, "Ngram");
 		job.setJarByClass(Ngram.class);
-		
+
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
 		job.setMapOutputKeyClass(NullWritable.class);
